f71827bdb17e57379d11ced500aed747c8a2da2a
jeongjin.heo <jeongjin.heo@dnsevercorp.com>
2025-01-13 18:53:31 +0900

    환경변수 타입 설정

--- src/types/react-app-env.d.ts
+++ src/types/react-app-env.d.ts
@@ -2,18 +2,10 @@
 
 declare namespace NodeJS {
   interface ProcessEnv {
-    /** flag for local dev mode */
     REACT_APP_USE_DEV_MODE: 'true' | 'false';
-    REACT_APP_JWT_TOKEN_DEBUG: string;
-    /** fixed hosts */
-    REACT_APP_LANDING_HOST: string;
-    REACT_APP_STATIC_HOST: string;
-    REACT_APP_ACCOUNT_HOST: string;
-    REACT_APP_CS_HOST: string;
-    /** version list */
-    REACT_APP_API_HOST_V1: string;
-    REACT_APP_DEBUG_API_HOST_V1: string;
-    REACT_APP_PUBLIC_WEBSOCKET_HOST_V1: string;
-    REACT_APP_PRIVATE_WEBSOCKET_HOST_V1: string;
+    REACT_APP_URL: string;
+    REACT_APP_API_DOMAIN: string;
+    REACT_APP_BOT_DOMAIN: string;
+    REACT_APP_WALLET_CONNECT_PROJECT_ID: string;
   }
 }
