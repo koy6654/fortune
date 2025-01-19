745a234bac2e65a272a58b01a16a4303dd1daf6e
serpiko <serpiko@hanmail.net>
2025-01-19 13:40:36 +0900

    hotfix: 인트로 불필요 폴더, 파일 삭제

--- src/features/intro/index.ts
+++ src/features/intro/index.ts
@@ -1,9 +1,1 @@
 export * from './components/Intro';
-
-export * from './models/intro.model';
-
-export * from './store';
-
-export * from './hooks/intro';
-export * from './hooks/mutations';
-export * from './hooks/queries';
