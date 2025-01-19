4d33662a1b727d30cebf35df6731a3931a150f4e
serpiko <serpiko@hanmail.net>
2025-01-19 13:39:06 +0900

    feature:Home 컴포넌트 포춘쿠키>스크롤오픈>팝업 띄우기 구현

---
+++ src/features/home/components/HomModal.tsx
@@ -0,0 +1,51 @@
+interface HomeModal {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export function HomModal(props: HomeModal) {
+  const { isOpen, onClose } = props;
+  const fortuneMessage = 'You will take a chance in the furture. Good Luck to you.';
+  const fortuneNumber = [12, 6];
+
+  const handleClose = () => {
+    if (onClose) {
+      onClose();
+    }
+  };
+
+  return isOpen ? (
+    <div className="fixed inset-0 bg-[rgba(139,69,19,0.7)] flex justify-center items-center z-50">
+      <div className="bg-white rounded-lg p-6 shadow-lg w-[90%] max-w-md">
+        <div data-name="fortune-message">
+          <header>Fortue Message</header>
+          <section>
+            <p>"</p>
+            <p>{fortuneMessage}</p>
+            <p>"</p>
+          </section>
+        </div>
+        <div data-name="fortune-number">
+          <header>Fortue Number</header>
+          <section className="flex justify-center gap-4 text-xl text-gray-700">
+            {fortuneNumber.map((number, index) => (
+              <>
+                <span key={`fortune-number-${index}`}>{number}</span>
+              </>
+            ))}
+          </section>
+        </div>
+        <div data-name="">
+          <span>icon1</span>
+          <span>icon2</span>
+          <span>icon3</span>
+        </div>
+        <div data-name="fortune-close">
+          <button type="button" onClick={handleClose}>
+            Close button
+          </button>
+        </div>
+      </div>
+    </div>
+  ) : null;
+}
--- src/features/home/components/Home.tsx
+++ src/features/home/components/Home.tsx
@@ -1,33 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as HomeMainFortuneWait } from 'assets/images/home/MainFortuneWait.svg';
 import { ReactComponent as HomeMainFortuneOpen } from 'assets/images/home/MainFortuneOpen.svg';
 import { ReactComponent as HomeMainScrollOpen } from 'assets/images/home/MainScrollOpen.svg';
 import { ReactComponent as HomeSmallClock } from 'assets/images/home/SmallClock.svg';
 import { useFortuneSyncStore } from 'features/auth';
 import { DEFAULT_FORTUNESYNC_FORTUNEINDEX } from 'consts/fortune';
+import { HomModal } from './HomModal';
 
 export const Home = () => {
   const fortuneSyncS = useFortuneSyncStore();
-  const fortuneIndex = fortuneSyncS.fortuneIndex;
+  const { fortuneIndex, isFortune } = fortuneSyncS;
+  const openTheScrollRestCount = DEFAULT_FORTUNESYNC_FORTUNEINDEX - fortuneIndex;
+  const [isFortuneClicked, setIsFortuneClicked] = useState(false);
+  const [isOpenScorllClicked, setIsOpenScrollClicked] = useState(false);
+
+  const handleClose = () => {
+    setIsOpenScrollClicked(false);
+  };
 
   return (
     <div className="flex-1 flex flex-col justify-center items-center relative">
-      <div className="absolute top-[-55px]">
-        <HomeMainFortuneWait />
-        {/* <HomeMainFortuneOpen /> */}
-        {/* <HomeMainScrollOpen /> */}
+      <div className={`absolute  ${isFortuneClicked ? 'top-[41px]' : 'top-[-55px]'}`}>
+        {Boolean(isFortune) ? (
+          <>
+            <div
+              className={`transition-all duration-300 ${isFortuneClicked ? 'hidden' : 'block'}`}
+              onClick={() => setIsFortuneClicked(true)}
+              style={{ cursor: 'pointer' }}
+            >
+              <HomeMainFortuneOpen />
+            </div>
+            <div
+              className={`transition-all duration-300 ${isFortuneClicked ? 'block' : 'hidden'}`}
+              onClick={() => setIsOpenScrollClicked(true)}
+              style={{ cursor: 'pointer' }}
+            >
+              <HomeMainScrollOpen />
+            </div>
+            <HomModal isOpen={!!isOpenScorllClicked} onClose={handleClose} />
+          </>
+        ) : (
+          <>
+            <HomeMainFortuneWait />
+          </>
+        )}
       </div>
       <div className="absolute bottom-[60px]">
-        <div className=" w-[270px] h-[56px] flex flex-row justify-center items-center px-6 py-6 bg-[#E5D5BA] rounded-[22px] border-[#737373] border-2 border-b-4">
-          <div className="text-[#737373] text-[22px] font-pridi font-semibold">Wait for your fortune</div>
-        </div>
-        {/* <div className=" w-[270px] h-[56px] flex flex-row justify-between items-center px-6 py-6 bg-[#ffc34b] rounded-[22px] border-[#956134] border-2 border-b-4">
-              <div className="text-black text-[22px] font-pridi font-semibold">Open the Scroll</div>
-              <div className="w-9 h-9 p-2.5 bg-[#956134] rounded-[100px] flex-col justify-center items-center gap-2.5 inline-flex">
-                <div className="text-[#fff5e8] text-[22px] font-pridi font-semibold">2</div>
-              </div>
-            </div> */}
-
+        {Boolean(isFortune) ? (
+          <div className=" w-[270px] h-[56px] flex flex-row justify-between items-center px-6 py-6 bg-[#ffc34b] rounded-[22px] border-[#956134] border-2 border-b-4">
+            <div className="text-black text-[22px] font-pridi font-semibold">Open the Scroll</div>
+            <div className="w-9 h-9 p-2.5 bg-[#956134] rounded-[100px] flex-col justify-center items-center gap-2.5 inline-flex">
+              <div className="text-[#fff5e8] text-[22px] font-pridi font-semibold">{openTheScrollRestCount}</div>
+            </div>
+          </div>
+        ) : (
+          <div className=" w-[270px] h-[56px] flex flex-row justify-center items-center px-6 py-6 bg-[#E5D5BA] rounded-[22px] border-[#737373] border-2 border-b-4">
+            <div className="text-[#737373] text-[22px] font-pridi font-semibold">Wait for your fortune</div>
+          </div>
+        )}
         <div className="flex flex-row justify-center mt-2">
           <span className="text-[#a48b78] text-sm font-pretendard font-medium">Scroll left today&nbsp;</span>
           <span className="text-[#956134] text-sm font-pretendard font-semibold">
