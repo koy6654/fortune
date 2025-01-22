c73ae8b56aac09b25ac0bcd28dce39cf4b8fb6e3
jeongjin.heo <jeongjin.heo@dnsevercorp.com>
2025-01-22 12:19:05 +0900

    feature: HomeModal 포춘 메세지 퍼블리싱

--- src/features/home/components/HomeModal.tsx
+++ src/features/home/components/HomeModal.tsx
@@ -1,6 +1,10 @@
 import { htmlToImage } from 'common/libs';
 import { useFortuneUserFortune } from 'features/services/queries';
 import { useEffect, useRef, useState } from 'react';
+import { ReactComponent as CloseIcon } from 'assets/images/home/home-modal-close-button.svg';
+import { ReactComponent as DownloadIcon } from 'assets/images/home/home-modal-icon-download.svg';
+import { ReactComponent as LinkIcon } from 'assets/images/home/home-modal-icon-link.svg';
+import { ReactComponent as TwitterIcon } from 'assets/images/home/home-modal-icon-twitter.svg';
 
 interface HomeModalProps {
   isOpen: boolean;
@@ -56,7 +60,7 @@ export function HomeModal(props: HomeModalProps) {
   return isOpen && loadedFortuneUserFortune ? (
     <div
       ref={captureRef}
-      className="fixed inset-0 bg-home-modal-radial flex justify-center items-center z-50"
+      className="w-full h-full fixed inset-0 bg-home-modal-radial flex justify-center items-center z-50"
       onClick={handleClose}
     >
       <div className="w-full pt-[115px] px-[75px] pb-[80px] text-[#FFF]" onClick={(event) => event.stopPropagation()}>
@@ -65,39 +69,50 @@ export function HomeModal(props: HomeModalProps) {
         ) : (
           <div data-name="fortune-result">
             <div data-name="fortune-message">
-              <header>
-                <hr />
-                <span>Fortue Message</span>
-                <hr />
+              <header className="flex items-center justify-between">
+                <hr className="w-[50px] border-t border-[#BC9D66]" />
+                <span className="text-[14px] text-[#F4DBBD] font-pridi leading-[27px]">
+                  <span className="font-light">Fortue</span> <span className="font-medium">Message</span>
+                </span>
+                <hr className="w-[50px] border-t border-[#BC9D66]" />
               </header>
-              <section>
-                <p>"</p>
-                <p>{fortuneMessage}</p>
-                <p>"</p>
+              <section className="text-[18px] font-semibold font-pridi flex flex-col items-center mt-[15px]">
+                <p className="text-[20px] leading-normal">“</p>
+                <p className="leading-[24px]">{fortuneMessage}</p>
+                <p className="text-[20px] leading-normal">“</p>
               </section>
             </div>
-            <div data-name="fortune-number">
-              <header>Fortue Number</header>
-              <section className="flex justify-center gap-4 text-xl text-gray-700">
+            <div data-name="fortune-number" className="mt-[36px]">
+              <header className="flex items-center justify-between">
+                <hr className="w-[50px] border-t border-[#BC9D66]" />
+                <span className="text-[14px] text-[#F4DBBD] font-pridi leading-[27px]">
+                  <span className="font-light">Fortue</span> <span className="font-medium">Number</span>
+                </span>
+                <hr className="w-[50px] border-t border-[#BC9D66]" />
+              </header>
+              <section className="flex flex-row justify-center items-center gap-10 font-pridi text-[20px] text-[#231815] leading-[27px] font-medium mt-[18px]">
                 {fortuneNumbers.map((number, index) => (
                   <span key={`fortune-number-${index}`}>
-                    <i>{number}</i>
+                    <i
+                      data-name="circle"
+                      className="flex justify-center items-center w-[55px] h-[55px] p-[10px] rounded-[50px] border border-[#E5AB38] bg-gradient-to-br from-[#FCF7EF] to-[#96938E] shadow-[0px_3px_4px_rgba(0,0,0,0.25)]"
+                    >
+                      {number}
+                    </i>
                   </span>
                 ))}
               </section>
             </div>
-            <div data-name="fortune-share">
-              <span onClick={() => handleShare('X')}>icon1 X</span>
-              <span onClick={() => handleShare('link')}>icon2 클립보드</span>
-              <span onClick={() => handleShare('download')}>icon3 다운로드</span>
+            <div data-name="fortune-share" className="mt-[80px] flex justify-around">
+              <TwitterIcon onClick={() => handleShare('X')} />
+              <LinkIcon onClick={() => handleShare('link')} />
+              <DownloadIcon onClick={() => handleShare('download')} />
             </div>
           </div>
         )}
 
-        <div data-name="fortune-close">
-          <button type="button" onClick={handleClose}>
-            Close button
-          </button>
+        <div data-name="fortune-close" className="flex justify-center mt-[34px]">
+          <CloseIcon onClick={handleClose} />
         </div>
       </div>
     </div>
