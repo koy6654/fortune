260bcf5c681980a8f28cbcd02fc57c0a1f4063fb
jeongjin.heo <jeongjin.heo@dnsevercorp.com>
2025-01-23 09:14:26 +0900

    feature: src/layout/components/Header.tsx 에 back 구현

--- src/layout/components/Header.tsx
+++ src/layout/components/Header.tsx
@@ -1,11 +1,23 @@
+import { ReactComponent as NavLeftArrow } from 'assets/images/nav-left-arrow.svg';
+import { useNavigate } from 'react-router-dom';
+
 interface HeaderProps {
   title: string;
   content: string;
 }
 
 export const Header = ({ title, content }: HeaderProps) => {
+  const navigate = useNavigate();
+
+  const handleMoveBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="h-[90px] w-full px-4 py-4">
+      <div className="fixed left-[20px] top-[44px]">
+        <NavLeftArrow onClick={handleMoveBack} />
+      </div>
       <div className="text-center text-black text-xl font-pridi-medium">{title}</div>
       <div
         className="text-center text-[#2b2b2b] text-xs font-normal font-pridi"
